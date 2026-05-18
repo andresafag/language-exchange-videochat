accdc83ae4b48680821dd12ccca4533163ed7fd1
andres acosta <andresfelipeacostagarcia34@gmail.com>
2026-05-18 11:53:52 -0400

    fix: landing page route

--- app.js
+++ app.js
@@ -18,6 +18,12 @@ const salas = [
     { id: 'fr', nombre: 'Francés', pais: 'Francia', img: 'https://speakswap-website.s3.us-east-1.amazonaws.com/public/eiffel.png' }
 ];
 
+// Route to render index page
+app.get('/', (req, res) => {
+  res.render('index', { salas });
+});
+
+
 app.get('/sala/:id', (req, res) => {
   const sala = { id: req.params.id, nombre: `Sala ${req.params.id}` };
   res.render('sala', { sala, secretIP: process.env.MY_TARGET_IP });
@@ -43,49 +49,6 @@ app.listen(PORT, () => {
   console.log(`✅ Express server running on port ${PORT}`);
 });
 
-// io.on('connection', (socket) => {
-//   socket.on('join-room', (roomId, userId, userName) => {
-//     const room = io.sockets.adapter.rooms.get(roomId);
-//     const numUsuarios = room ? room.size : 0;
-
-//     if (numUsuarios >= 10) {
-//       socket.emit('sala-llena', roomId);
-//       return;
-//     }
-
-//     socket.join(roomId);
-
-//     // Guardar datos del usuario en el socket
-//     socket.data.userId = userId;
-//     socket.data.userName = userName;
-
-//     // Emitir lista completa de usuarios a todos en la sala
-//     io.to(roomId).emit('room-users', getUsersInRoom(roomId));
-
-//     // Manejar desconexión
-//     socket.on('disconnect', () => {
-//       io.to(roomId).emit('room-users', getUsersInRoom(roomId));
-//     });
-//   });
-// });
-
-// // Helper para obtener usuarios de la sala
-// function getUsersInRoom(roomId) {
-//   const room = io.sockets.adapter.rooms.get(roomId);
-//   const usuarios = [];
-//   if (room) {
-//     for (const peerId of room) {
-//       const peerSocket = io.sockets.sockets.get(peerId);
-//       if (peerSocket && peerSocket.data) {
-//         usuarios.push({
-//           id: peerSocket.data.userId,
-//           name: peerSocket.data.userName
-//         });
-//       }
-//     }
-//   }
-//   return usuarios;
-// }
 
 
 
